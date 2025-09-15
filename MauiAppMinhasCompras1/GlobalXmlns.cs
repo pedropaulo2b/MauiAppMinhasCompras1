19cd21a01dea4e3092f2675d2e1c431b2cc3e94e
pedro <pedropbb2324@gmail.com>
2025-09-15 05:36:43 -0300

    Adicionar arquivos de projeto.

---
+++ MauiAppMinhasCompras1/GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiAppMinhasCompras1")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "MauiAppMinhasCompras1.Pages")]
