bb57ebe25102eef48d3bdd8f8f8daf7fed79f890
pedro <pedropbb2324@gmail.com>
2025-09-15 11:18:44 -0300

    nova versao funcionando

--- MauiAppMinhasCompras1/App.xaml.cs
+++ MauiAppMinhasCompras1/App.xaml.cs
@@ -1,13 +1,36 @@
-﻿namespace MauiAppMinhasCompras1
+﻿using MauiAppMinhasCompras1.Helpers;
+
+namespace MauiAppMinhasCompras1
 {
     public partial class App : Application
     {
+        static SQLiteDatabaseHelper? _db;
+
+        public static SQLiteDatabaseHelper Db
+        {
+            get
+            {
+                if (_db == null)
+                {
+                    string path = Path.Combine(
+                        Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData),"banco_sqlit_compras.db3");
+                    _db = new SQLiteDatabaseHelper(path);
+
+                }
+                return _db;
+
+            }
+
+        }
+       
         public App()
         {
             InitializeComponent();
+        }
 
-            //MainPage = new AppShell();
-            MainPage = new NavigationPage(new Views.ListaProduto());
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            return new Window(new NavigationPage(new Views.ListaProduto()));
         }
     }
 }
--- MauiAppMinhasCompras1/Helpers/SQLiteDatabaseHelper.cs
+++ MauiAppMinhasCompras1/Helpers/SQLiteDatabaseHelper.cs
@@ -3,11 +3,11 @@ using SQLite;
 
 namespace MauiAppMinhasCompras1.Helpers
 {
-    internal class SQLiteDatabaseHelper
+    public class SQLiteDatabaseHelper
     {
         readonly SQLiteAsyncConnection _conn;
 
-        public SQLiteDatabaseHelper(string path) 
+        public SQLiteDatabaseHelper(string path)     
         {
             _conn = new SQLiteAsyncConnection(path);
             _conn.CreateTableAsync<Produto>().Wait();
@@ -17,29 +17,28 @@ namespace MauiAppMinhasCompras1.Helpers
             return _conn.InsertAsync(p);
         }
 
-        public Task<List<Produto>> update(Produto p) 
+        public Task<int> update(Produto p) 
         {
             string sql = "UPDATE Produto SET Desquicao = ?, Quantidade = ?, Preco = ? WHERE Id = ?";
-            return _conn.QueryAsync<Produto>(
+            return _conn.ExecuteAsync(
                 sql, p.Desquicao, p.Quantidade, p.Preco, p.Id
-                );
+            );
         }
 
-        public Task<int>  delete(int id) 
+        public Task<int> delete(int id) 
         {
-           return _conn.table<Produto>().DeleteAsync(i => i.Id == id);
+           return _conn.DeleteAsync<Produto>(id);
         }   
 
-        public Task<int> getAll() 
+        public Task<List<Produto>> getAll() 
         { 
            return _conn.Table<Produto>().ToListAsync();
         }
 
         public Task<List<Produto>> Search(string q) 
-        
         {
-            string sql = "SELECT * Produto WHERE descricao LIKE '%" + q + "%' ";
-            return _conn.QueryAsync<Produto>(sql);
+            string sql = "SELECT * FROM Produto WHERE Desquicao LIKE ?";
+            return _conn.QueryAsync<Produto>(sql, $"%{q}%");
         }
-
-    }
+    } // <-- Corrigido: fechando a classe corretamente
+} // <-- Corrigido: fechando o namespace corretamente
